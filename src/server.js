753caf4de165d49a289000fa1781c277d0dee803
Shuvrojit Biswas <jsuvro17@gmail.com>
2023-07-21 21:36:21 +0600

    Setup server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
+import bodyParser from "body-parser"
 import express from "express"
 
+
 const app = express()
+
+bodyParser.json()
+bodyParser.urlencoded({extended: true})
+
+app.get("/", (req, res) => {
+  res.status(200)
+  res.json({message: "home"})
+})
 
 export default app
